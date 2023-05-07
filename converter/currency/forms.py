8c98a660f2989bde957fdcdf64706cdbc45feddb
Maksym Vasyltsiv <kasta027@gmail.com>
2023-05-07 12:07:54 +0300

    create form and update views

--- converter/currency/forms.py
+++ converter/currency/forms.py
@@ -6,6 +6,7 @@ class CurrencyForm(forms.Form):
     currency1 = forms.ChoiceField()
     currency2 = forms.ChoiceField()
     count = forms.FloatField()
+    round_num = forms.BooleanField(label='Round up to: 0.00', required=False)
 
     def __init__(self, *args, **kwargs):
         choices = kwargs.pop('choices', None)
--- converter/currency/views.py
+++ converter/currency/views.py
@@ -1,23 +1,29 @@
 from django.shortcuts import render
 from .forms import CurrencyForm
 from requests import get
+from decimal import Decimal, ROUND_UP
 
 
 def index(request):
     list_choices = [
-        (1, 'usdt'),
-        (2, 'btc'),
-        (3, 'uah'),
+        (1, 'Українська гривня')
     ]
     response = get('https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?json').json()
     for item in response:
-        list_choices.append((item['r030'], item['cc']))
+        list_choices.append((item['rate'], item['txt']))
 
     if request.method == 'POST':
-        form = CurrencyForm(request.POST, choices=list_choices, a=1)
-        currency1 = request.POST['currency1']
-        currency2 = request.POST['currency2']
+        form = CurrencyForm(request.POST, choices=list_choices)
+        if form.is_valid():
+            currency1 = Decimal(form.cleaned_data['currency1'])
+            currency2 = Decimal(form.cleaned_data['currency2'])
+            count = Decimal(form.cleaned_data['count'])
+            is_round = form.cleaned_data['round_num']
+            result = count * currency1 / currency2
+            if is_round:
+                result = result.quantize(Decimal('0.00'), rounding=ROUND_UP)
+
+        return render(request, 'index.html', {'form': form, 'result': result})
     else:
         form = CurrencyForm(choices=list_choices)
-
-    return render(request, 'index.html', {'form': form})
+        return render(request, 'index.html', {'form': form})
